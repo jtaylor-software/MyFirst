361d517fa02454e5f86c3897ef4808728dd36e04
Jeremy Taylor <jtaylor@jctaylor.org>
2025-03-04 22:56:26 -0600

    Add ByeView

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,18 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by Jeremy Taylor on 3/4/25.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World")
+    }
+}
+
+#Preview {
+    ByeView()
+}
